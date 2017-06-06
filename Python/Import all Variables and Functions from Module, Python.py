63c1dc0b56a41de008a809cf3bddf19b7e6433e4
Fallenour <Logan.Hicks@live.com>
2017-06-06 06:51:45 -0500

    Add files via upload
    Import all Variables and Functions from a Module, Python

---
+++ Python/Import all Variables and Functions from Module, Python.py
@@ -0,0 +1,27 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet imports all the functions and variables from the 
+os module in Python.
+
+##############################################################
+
+'''
+
+
+
+
+
+from os import *
