19ac46019b92f8f4002bd5d150136e302d4c4ab2
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:07:08 -0500

    Add files via upload
    Simple Function, IF ELSE Statement, Python
    
    Simple Function, IF ELSE ELIF, Python
    
    This particular option is useful for checking if options (Check Boxes) are selected or not, and conducting "X" action. This is useful when working with UIs

---
+++ Python/Simple Function, IF ELSE statement, Python.py
@@ -0,0 +1,40 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a simple Function with IF ELSE statement
+in python.
+
+##############################################################
+
+'''
+
+
+
+
+
+box1 = "Checked"
+box2 = "Unchecked"
+
+def function1():
+    if box1 == "Checked":
+        return True
+    else:             
+        return
+
+def function2():
+    if box2 == "Unchecked":
+        return True
+    else:             
+        return
