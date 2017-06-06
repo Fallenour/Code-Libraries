a072fd4a33844635ba8b7b35dadd3852bff50cf9
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:02:23 -0500

    Add files via upload
    Multiline Comment, Python

---
+++ Python/Multiple Line Comment, Python.py
@@ -0,0 +1,29 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/2/2017
+Date Last Updated: 5/2/2017
+Code Source: http://stackoverflow.com/questions/21253148/multiple-line-comment-in-python
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a multiple line comment in python.
+
+##############################################################
+
+'''
+
+
+
+
+
+'''
+This is a multiline
+comment. I can type here whatever I want.
+'''
