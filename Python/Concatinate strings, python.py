5e31d0dbca1b06aa2542c69be780ad539615417b
Fallenour <Logan.Hicks@live.com>
2017-06-06 06:44:12 -0500

    Add files via upload
    Concatinate Strings, Python

---
+++ Python/Concatinate strings, python.py
@@ -0,0 +1,30 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/5/2017
+Date Last Updated: 5/5/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet concatinates strings in python.
+
+##############################################################
+
+'''
+
+
+
+
+
+string_1 = "C:/"
+string_2 = "Windows/"
+string_3 = "Windows32/"
+
+print "%s%s%s" % (string_1, string_2, string_3)
