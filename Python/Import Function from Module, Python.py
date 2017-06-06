d7c3d29b0ad636f19d7b7fb4edda2f6b9faa635d
Fallenour <Logan.Hicks@live.com>
2017-06-06 06:57:14 -0500

    Add files via upload
    Import Function from Module, Python
    
    This function, makedir, is from Module OS. It is used to create directories. It is especially useful if you are creating files and would like to structure them.

---
+++ Python/Import Function from Module, Python.py
@@ -0,0 +1,29 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet imports a function from a module in python.
+
+This example imports the makedirs function from the os module
+in Python.
+
+##############################################################
+
+'''
+
+
+
+
+
+from os import makedirs
