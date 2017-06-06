4a9ce2ead3e51f64a5a7b8804f0c08d279c977f7
Fallenour <Logan.Hicks@live.com>
2017-06-06 06:55:13 -0500

    Add files via upload
    Import DateTime LIbrary, Get Time now

---
+++ Python/Import DateTime Library, get time now, python.py
@@ -0,0 +1,32 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet imports the datetime library and displays the date
+& time right now in python.
+
+Format is as follows:
+
+2017-05-06 11:56:41.262460
+
+##############################################################
+
+'''
+
+
+
+
+from datetime import datetime
+now = datetime.now()
+print datetime.now()
