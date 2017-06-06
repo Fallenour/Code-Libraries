03473be68c40316b6249377b5e019ef5a953afd9
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:02:59 -0500

    Add files via upload
    For Loop, Python

---
+++ Python/Simple For Loops, Strings and Numbers, Python.py
@@ -0,0 +1,53 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/7/2017
+Date Last Updated: 5/7/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates two separate for loops. The first for loop
+multiplies each number in the list by 2, and prints out that 
+value. In the second for loop, it prints out each color in the
+list.
+
+Output from for loop #1:
+
+2
+18
+6
+16
+10
+14
+
+Output from for loop #2: 
+
+red
+green
+blue
+purple
+
+##############################################################
+
+'''
+
+
+
+
+
+my_list = [1,9,3,8,5,7]
+
+for number in my_list:
+    print 2*number
+	
+
+colors = ["red", "green", "blue", "purple"]
+for color in colors:
+    print(color)
