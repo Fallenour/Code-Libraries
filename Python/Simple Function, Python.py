ba850c4f194a2642f7b56a90cdb2d88045b03bd7
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:08:24 -0500

    Add files via upload
    Simple Functions, Boolean Logic Check, Python

---
+++ Python/Simple Function, Python.py
@@ -0,0 +1,35 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a simple FUNCTION in python.
+
+##############################################################
+
+'''
+
+
+
+
+
+def function1():
+    if True:
+        return "Success #1"
+
+def function2():
+    if True:
+        return "Success #2"
+
+print function1()
+print function2()
