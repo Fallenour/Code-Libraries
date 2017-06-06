6722f44013f55f5d2c9f653bb7129d9c69d37970
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:10:49 -0500

    Add files via upload
    Simple List, Create Empty List, Append 4 Items to List, Python

---
+++ Python/Simple List, Appends Item to list, Python.py
@@ -0,0 +1,38 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/7/2017
+Date Last Updated: 5/7/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a list thats empty, and then appends
+four items to it in Python.
+
+##############################################################
+
+'''
+
+
+
+
+
+Itemlist = [] 
+Itemlist.append("Item1")
+suitcase.append("Item2")
+suitcase.append("Item3")
+suitcase.append("Item4")
+
+
+
+list_length = len(Itemlist) # Set this to the length of suitcase
+
+print "There are %d items in the Itemlist." % (list_length)
+print Itemlist
