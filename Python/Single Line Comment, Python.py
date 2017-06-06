ce6090797042e20e393d9f52db3258b5254990b3
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:33:20 -0500

    Add files via upload
    Single Line Comment, Python

---
+++ Python/Single Line Comment, Python.py
@@ -0,0 +1,26 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/2/2017
+Date Last Updated: 5/2/2017
+Code Source: https://docs.python.org/2/tutorial/introduction.html
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a single line comment in python.
+
+##############################################################
+
+'''
+
+
+
+
+
+#This is a single line comment.
