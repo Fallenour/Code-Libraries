dfb3999e828ca94a8ec1bdb500ed5b42ab2d3ab6
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:32:32 -0500

    Add files via upload
    Simple List, Slice Strings, Python
    
    Is is incredibly useful wwhen you want to work with text. For instance, logs. You can use this to parse logs by slicing the logs at specific points on the log currently loaded to a variable. Note this is only useful if the strings are of the same size, IE IP Addresses, MAC Addresses, phone numbers, zipcodes, etc

---
+++ Python/Simple List, slices string into items, Python.py
@@ -0,0 +1,31 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/7/2017
+Date Last Updated: 5/7/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet takes a string, and slices it up into 3 items,
+and puts them into a new list called Items. 
+
+##############################################################
+
+'''
+
+
+
+
+
+Items = "ABCDEFGHIJ"
+
+Item1  = Items[:3]
+Item2  = Items[3:6]
+Item3 = Items[6:10]
