99609f456687a48ce4f8f0f8713b283ba47deac8
Fallenour <Logan.Hicks@live.com>
2017-06-06 06:55:45 -0500

    Add files via upload
    Import DateTime Library, Python

---
+++ Python/Import DateTime Library, Python.py
@@ -0,0 +1,25 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet imports the datetime library in python.
+
+##############################################################
+
+'''
+
+
+
+
+from datetime import datetime
