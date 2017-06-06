4f59ec48dcc1eb6366cc4159b4f6b53113a7fa57
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:01:49 -0500

    Add files via upload
    Imports all content from Module, displays all Functions and Variables imported, Python
    
    This is especially important to understand, as it is utilized to ensure you arent using a preallocated variable, it shows all the additional functions imported, and most importantly, it shows you the additional potential capabilities provided by the module

---
+++ Python/Import Module and show everything imported, Python.py
@@ -0,0 +1,30 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet imports all the functions and variables from the 
+os module in Python, and then displays everything imported in 
+a list as a printed output so you can see what was imported.
+
+##############################################################
+
+'''
+
+
+
+
+
+import os
+everything = dir(os)
+print everything
