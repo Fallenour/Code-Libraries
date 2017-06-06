01e49d872c8cddd17c90d8a456fbc87775b1c96b
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:28:48 -0500

    Add files via upload
    Simple List, Creates Empty List, Does Variable Check, Prints List, Python

---
+++ Python/Simple List, Python.py
@@ -0,0 +1,33 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a list of 4 animals, setting each animal
+in a list from 0 to 3.
+
+##############################################################
+
+'''
+
+
+
+
+
+zoo_animals = ["pangolin", "cassowary", "sloth", "zebra"];
+
+if len(zoo_animals) > 3:
+	print "The first animal at the zoo is the " + zoo_animals[0]
+	print "The second animal at the zoo is the " + zoo_animals[1]
+	print "The third animal at the zoo is the " + zoo_animals[2]
+	print "The fourth animal at the zoo is the " + zoo_animals[3]
