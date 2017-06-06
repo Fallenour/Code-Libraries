6da8a99871f3a0a904dc8efc120ed1d1d778c5cd
Fallenour <Logan.Hicks@live.com>
2017-06-06 07:09:33 -0500

    Add files via upload
    IF Statement, Python

---
+++ Python/Simple IF statement, Python.py
@@ -0,0 +1,28 @@
+'''
+
+Author: Logan Hicks, (Fallenour)
+Contact Information: Fallenour@yahoo.com, Skype: Fallenour
+Date Created: 5/6/2017
+Date Last Updated: 5/6/2017
+Code Source: Derivative
+Coded In: Python
+
+
+
+
+
+############   Intended Purpose of this program   ############
+
+This snippet creates a simple IF statement in python.
+
+##############################################################
+
+'''
+
+
+
+
+
+answer = "Checked"
+if answer == "Checked":
+    print "This is a sample print statement generated due to answer equaling Checked"
